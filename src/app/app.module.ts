d334f5fc173cdaba94e5f8d82006510a67b7fa7a
DiegolPl <diegopdl1992@gmail.com>
2022-10-20 10:27:10 -0300

    Agrego servicio con informacion temporal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/parte-publica/header/header.component';
 import { BannerComponent } from './componentes/parte-publica/banner/banner.component';
 import { LoginModalComponent } from './componentes/parte-publica/login-modal/login-modal.component';
+import { PerfilComponent } from './componentes/parte-publica/perfil/perfil.component';
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BannerComponent,
-    LoginModalComponent
+    LoginModalComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
