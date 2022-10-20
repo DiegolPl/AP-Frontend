ac715ca966189e05918db0c2d107b0d9c22a687b
DiegolPl <diegopdl1992@gmail.com>
2022-10-20 10:03:51 -0300

    Agrego componente login-modal de parte publica

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/parte-publica/header/header.component';
 import { BannerComponent } from './componentes/parte-publica/banner/banner.component';
+import { LoginModalComponent } from './componentes/parte-publica/login-modal/login-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BannerComponent
+    BannerComponent,
+    LoginModalComponent
   ],
   imports: [
     BrowserModule,
