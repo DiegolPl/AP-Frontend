78cd85f1941d6774d01f2ff63d72d11c3c4a44f2
DiegolPl <diegopdl1992@gmail.com>
2022-10-20 10:28:11 -0300

    Agrego componente perfil de parte publica

---
+++ src/app/componentes/parte-publica/perfil/perfil.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { DataTemporalService } from 'src/app/service/parte-interna/data-temporal.service';
+
+@Component({
+  selector: 'app-perfil',
+  templateUrl: './perfil.component.html',
+  styleUrls: ['./perfil.component.css']
+})
+export class PerfilComponent implements OnInit {
+
+  miPorfolio:any = [];
+  faTimes = faTimes;
+
+  constructor(private dataTemporalService:DataTemporalService) { }
+
+  ngOnInit(): void {
+    this.dataTemporalService.getData().subscribe(data => this.miPorfolio = data);
+  }
+
+}
