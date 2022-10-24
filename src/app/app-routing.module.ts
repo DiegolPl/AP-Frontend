3620abedf05fd17dd5e8580216c56516d27fd493
DiegolPl <diegopdl1992@gmail.com>
2022-10-24 15:42:09 -0300

    Agrego ruteo de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PartePrivadaComponent } from './componentes/parte-privada/parte-privada/parte-privada.component';
+import { PartePublicaComponent } from './componentes/parte-publica/parte-publica/parte-publica.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'index', component: PartePublicaComponent},
+  {path: '', component: PartePublicaComponent},
+  {path: 'dashboard', component: PartePrivadaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
